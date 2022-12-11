f3be791cd5cfa95510d3216ee9ea456391ca2adb
fauzirentah <fauzi.rentah@gmail.com>
2022-12-11 20:47:17 +0800

    goto github & intent

--- app/src/main/java/com/ict602/bmicalculator/AboutActivity.java
+++ app/src/main/java/com/ict602/bmicalculator/AboutActivity.java
@@ -1,19 +1,29 @@
 package com.ict602.bmicalculator;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.text.HtmlCompat;
 
 public class AboutActivity extends AppCompatActivity {
     Button backBtn;
+    Button repoBtn;
+    String htmlText = "<a href='https://github.com/fauzirentah/BMICalculator'>https://github.com/fauzirentah/BMICalculator</a>";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
         backBtn = findViewById(R.id.back_btn);
+        repoBtn = findViewById(R.id.repo_btn);
+
+        TextView htmlToTextView = findViewById(R.id.about_copyright);
+        htmlToTextView.setText(HtmlCompat.fromHtml(htmlText, 0));
 
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,5 +32,18 @@ public class AboutActivity extends AppCompatActivity {
             }
         });
 
+        repoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gotoRepo();
+            }
+        });
+
     }
+
+    public void gotoRepo() {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/fauzirentah/BMICalculator"));
+        startActivity(browserIntent);
+    }
+
 }
